abc99f78129cbc993fd9872aeb6615846ac25e6c
Mateusz Choma <chomamateusz@gmail.com>
2018-11-12 21:20:19 +0100

    Client - replaced local feathers URL with deployed on now.sh

--- client/src/feathers.js
+++ client/src/feathers.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import feathers from '@feathersjs/client'
 import auth from '@feathersjs/authentication-client'
 
-const socket = io('localhost:3030')
+const socket = io('https://feathers-js-meet-js-13-11-2018.now.sh/')
 const app = feathers()
 
 app.configure(feathers.socketio(socket))
